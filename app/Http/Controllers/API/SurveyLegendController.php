b685e3892842e21fc900494fbe3024984889d1b5
korolev <evgen.korolev@quardex.com>
2020-04-30 11:39:04 +0300

    start work with API

--- app/Http/Controllers/API/SurveyLegendController.php
+++ app/Http/Controllers/API/SurveyLegendController.php
@@ -7,7 +7,24 @@ use App\Http\Controllers\Controller;
 
 class SurveyLegendController extends Controller
 {
-    public function sendMailForList () {
-        var_dump(123123);die;
+    public function saveSurveyAnswers (Request $request) {
+        //if(!empty($_GET) || !empty($_POST)){
+
+            //$json = json_decode($request->getContent(), true);
+        $data_json = file_get_contents('php://input');
+        $data = json_decode($data_json, true);
+            $name = $request->input();
+            $get = $_GET;
+            $post = $_POST;
+            $path_get = './get.txt';
+            $path_post = './post.txt';
+            $path_name = './name.txt';
+            $path_json = './json.txt';
+            file_put_contents($path_get,$get);
+            file_put_contents($path_post,$post);
+            file_put_contents($path_json,$data);
+            file_put_contents($path_name,$name);
+        //}
+       // var_dump(file_put_contents($path_get,$get));die;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/send-mails', 'API\SurveyLegendController@sendMailForList');
+Route::match(array('GET', 'POST'),'/save-survey-answers', 'API\SurveyLegendController@saveSurveyAnswers');

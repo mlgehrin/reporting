7125a8cb9699e481907cba04ee426b925c92d175
korolev <evgen.korolev@quardex.com>
2020-05-28 17:51:55 +0300

    finish create functional for add dynamic survay id

--- app/Http/Controllers/API/SurveyLegendController.php
+++ app/Http/Controllers/API/SurveyLegendController.php
@@ -96,7 +96,6 @@ class SurveyLegendController extends Controller
                         if($company->id_form_peer_reflection == $data['form_response']['form_id']){
                             $peer_list_item_id = $ids[1];
                             $peer_list_item = PeerList::find($peer_list_item_id);
-                            //var_dump($peer_list_item_id, $participant_id);die;
                             if(!empty($peer_list_item)){
                                 $peer_list_item->success_peer_reflection = 1;
                                 $save = $peer_list_item->save();
@@ -106,92 +105,9 @@ class SurveyLegendController extends Controller
                             }
                         }
                     }
-
                 }
                 response('',200);die;
             }
         }
-
-        // Self Reflection for participant (typeform)
-        /*if($data['form_response']['form_id'] == 'A6kAYZ'){
-            if (!empty($data['form_response']['hidden']['participant_id'])){
-                $participant_id = htmlentities(trim($data['form_response']['hidden']['participant_id']));
-                $participant = Participant::find($participant_id);
-                if(!empty($participant)){
-                    $participant->success_self_reflection = 1;
-                    $participant->save();
-                }
-                response('',200);
-            }
-        }*/
-
-        // Peer Reflection for peer list (typeform)
-        /*if($data['form_response']['form_id'] == 'TpGzxP'){
-            if(!empty($data['form_response']['hidden']['participant_id'])){
-                $check_peer_list = strripos($data['form_response']['hidden']['participant_id'], '-');
-                if($check_peer_list !== false){
-                    $survey_participant_id = htmlentities(trim($data['form_response']['hidden']['participant_id']));
-                    $ids = explode('-', $survey_participant_id);
-                    $participant_id = $ids[0];
-                    $peer_list_item_id = $ids[1];
-                    $peer_list_item = PeerList::find($peer_list_item_id);
-                    if(!empty($peer_list_item)){
-                        $peer_list_item->success_peer_reflection = 1;
-                        $peer_list_item->save();
-                    }
-                }
-                response('',200);
-            }
-        }*/
-
-        // Peer Collection for participant (typeform)
-        /*if($data['form_response']['form_id'] == 'VxT92U'){
-            if(!empty($data['form_response']['hidden']['participant_id'])){
-                $check_peer_list = strripos($data['form_response']['hidden']['participant_id'], '-');
-                if ($check_peer_list === false){
-                    $participant_id = htmlentities(trim($data['form_response']['hidden']['participant_id']));
-                    $participant = Participant::find($participant_id);
-                    if(!empty($participant) && !empty($data['form_response']['answers'])){
-                        $participant->success_peer_reflection = 1;
-
-                        $answers = $data['form_response']['answers'];
-                        $emails = array();
-                        foreach ($answers as $answer) {
-                            if($answer['type'] == 'email' && !empty($answer['email'])){
-                                $emails[] = htmlentities(trim($answer['email']));
-                            }
-                        }
-                        $emails = array_unique($emails);
-                        if(!empty($emails)){
-                            foreach ($emails as $email){
-                                $count_duplicate = PeerList::where('email', $email)->count();
-                                if($count_duplicate >= 5){
-                                    continue;
-                                }else{
-                                    $peer_list_item = new PeerList();
-                                    $peer_list_item->participant_id = $participant_id;
-                                    $peer_list_item->email = $email;
-                                    $peer_list_item->counter_sending_peer_reflection = $peer_list_item->counter_sending_peer_reflection + 1;
-                                    $peer_list_item->save();
-                                    $peer_list_item_id = $peer_list_item->id;
-                                    $id = $participant_id . '-' . $peer_list_item_id;
-                                    $template_path = 'mailing.peerList.peerReflection';
-                                    $company = Company::find($participant->company_id)->first();
-                                    $id_form_peer_reflection = $company->id_form_peer_reflection;
-                                    //var_dump($id_form_peer_reflection);die;
-                                    if(!empty($id_form_peer_reflection)){
-                                        SendEmail::dispatch($email, $id, $template_path, $id_form_peer_reflection);
-                                    }
-                                }
-                            }
-                            $participant->status_peer_list = 1;
-                        }
-                        $participant->save();
-                    }
-                }
-                response('',200);
-            }
-        }*/
-
     }
 }
--- app/Http/Controllers/Mailing/ReminderController.php
+++ app/Http/Controllers/Mailing/ReminderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Mailing;
 
+use App\Models\Company;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Participant;
@@ -18,9 +19,13 @@ class ReminderController extends Controller
         if(count($participants)){
             foreach ($participants as $participant) {
                 $template_path = 'mailing.reminder.selfReflection';
+                $company = Company::find($participant->company_id);
+                $id_form_self_reflection = $company->id_form_self_reflection;
                 $participant->reminder_self_reflection = $participant->reminder_self_reflection + 1;
                 $participant->save();
-                SendEmail::dispatch($participant->email, $participant->id, $template_path);
+                if(!empty($id_form_self_reflection)){
+                    SendEmail::dispatch($participant->email, $participant->id, $template_path, $id_form_self_reflection);
+                }
             }
         }
 
@@ -33,12 +38,15 @@ class ReminderController extends Controller
         if(count($participants)){
             foreach ($participants as $participant) {
                 $template_path = 'mailing.reminder.peerCollection';
+                $company = Company::find($participant->company_id);
+                $id_form_peer_collection = $company->id_form_peer_collection;
                 $participant->reminder_peer_reflection = $participant->reminder_peer_reflection + 1;
                 $participant->save();
-                SendEmail::dispatch($participant->email, $participant->id, $template_path);
+                if(!empty($id_form_peer_collection)){
+                    SendEmail::dispatch($participant->email, $participant->id, $template_path, $id_form_peer_collection);
+                }
             }
         }
-
     }
 
     public static function reminderForPeerReflection(){
@@ -50,12 +58,16 @@ class ReminderController extends Controller
         if(count($peer_list)){
             foreach ($peer_list as $peer_list_item) {
                 $template_path = 'mailing.reminder.peerReflection';
+                $participant = Participant::find($peer_list_item->participant_id);
+                $company = Company::find($participant->company_id);
+                $id_form_peer_reflection = $company->id_form_peer_reflection;
                 $peer_list_item->reminder_peer_reflection = $peer_list_item->reminder_peer_reflection + 1;
                 $peer_list_item->save();
-                SendEmail::dispatch($peer_list_item->email, $peer_list_item->id, $template_path);
+                if(!empty($id_form_peer_reflection)){
+                    SendEmail::dispatch($peer_list_item->email, $peer_list_item->id, $template_path, $id_form_peer_reflection);
+                }
             }
         }
-
     }
 
     public static function unsubscribeDublicateEmails(){
@@ -67,6 +79,5 @@ class ReminderController extends Controller
                 PeerList::where('email', $item->email)->update(['unsubscribed_peer_reflection' => 1]);
             }
         }
-
     }
 }

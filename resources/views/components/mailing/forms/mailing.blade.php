ea5f12e24966ba7be15746e22844f5dd3b41217a
korolev <evgen.korolev@quardex.com>
2020-05-29 15:51:59 +0300

    start create frontend for survey form id

--- resources/views/components/mailing/forms/mailing.blade.php
+++ resources/views/components/mailing/forms/mailing.blade.php
@@ -1,3 +1,4 @@
+<?php //var_dump($companies);die; ?>
 <div class="block-mailing row">
     <div class="col-10">
         @if(@isset($companies))
@@ -7,8 +8,7 @@
                         <label for="id-company">Сhoose company</label>
                         <select class="form-control" name="id-company" id="id-company">
                             @foreach($companies as $company)
-                                <option
-                                        value="{{ $company->id }}">{{ $company->name }}</option>
+                                <option value="{{ $company->id }}">{{ $company->name }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -16,6 +16,15 @@
                         <button id="start-mailing" class="btn btn-primary">Send</button>
                     </div>
 
+                    <div>
+                        <lable><input name="id_self_reflection" data-company-id="{{ $companies[0]->id }}" type="text" value="{{ !empty($companies[0]->id_form_self_reflection) ? $companies[0]->id_form_self_reflection : '' }}">Self Reflection ID</lable>
+                    </div>
+                    <div>
+                        <lable><input name="id_peer_collection" data-company-id="{{ $companies[0]->id }}" type="text" value="{{ !empty($companies[0]->id_form_peer_collection) ? $companies[0]->id_form_peer_collection : '' }}">Peer Collection ID</lable>
+                    </div>
+                    <div>
+                        <lable><input name="id_peer_reflection" data-company-id="{{ $companies[0]->id }}" type="text" value="{{ !empty($companies[0]->id_form_peer_reflection) ? $companies[0]->id_form_peer_collection : '' }}">PeerReflection ID</lable>
+                    </div>
                     <div class="form-group ml-auto">
                         <button id="remove-company" class="btn btn-outline-danger">Remove company</button>
                     </div>

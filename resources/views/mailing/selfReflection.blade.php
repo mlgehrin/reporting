f24ae4ad1e77e2f1f38ad415e40edac1a4715530
korolev <evgen.korolev@quardex.com>
2020-08-13 10:51:11 +0300

    fix text

--- resources/views/mailing/selfReflection.blade.php
+++ resources/views/mailing/selfReflection.blade.php
@@ -13,5 +13,5 @@
 </div>
 <br>
 <div>
-    <h6><a style="color: grey;" href="http://161.35.3.39/unsubscribe/self-reflection/{{ $user_id }}">Unsubscribe from a reminder</a></h6>
+    <h6><a style="color: grey;" href="http://161.35.3.39/unsubscribe/self-reflection/{{ $user_id }}">Unsubscribe</a></h6>
 </div>
